cd5086c28f2df92dbe10df8688190982c54a5cab
mrlynn <merlynn@gmail.com>
2019-08-05 08:57:46 -0400

    add configuration

--- main.go
+++ main.go
@@ -1,69 +1,36 @@
 package main
 
 import (
-	"crypto/rand"
-	"encoding/base64"
-	"encoding/json"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
-
+	"./config"
 	"github.com/gin-gonic/gin"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var cred Credentials
-var conf *oauth2.Config
-
-// Credentials holds client_id and client_secret from google auth
-type Credentials struct {
-	Cid     string `json:"cid"`
-	Csecret string `json:"csecret"`
-}
-
-// RandToken generates a random @l length token.
-func RandToken(l int) (string, error) {
-	b := make([]byte, l)
-	if _, err := rand.Read(b); err != nil {
-		return "", err
-	}
-	return base64.StdEncoding.EncodeToString(b), nil
-}
-
-func getLoginURL(state string) string {
-	return conf.AuthCodeURL(state)
-}
-
-// initialize credentials from file creds.json
-func init() {
-	file, err := ioutil.ReadFile("./creds.json")
-	if err != nil {
-		log.Printf("File error: %v\n", err)
-		os.Exit(1)
-	}
-	if err := json.Unmarshal(file, &cred); err != nil {
-		log.Println("unable to marshal data")
-		return
-	}
-
-	conf = &oauth2.Config{
-		ClientID:     cred.Cid,
-		ClientSecret: cred.Csecret,
-		RedirectURL:  "http://127.0.0.1:9090/auth",
-		Scopes: []string{
-			"https://www.googleapis.com/auth/userinfo.email", // You have to select your own scope from here -> https://developers.google.com/identity/protocols/googlescopes#google_sign-in
-		},
-		Endpoint: google.Endpoint,
-	}
-}
-
-// IndexHandler handles the location /.
-func IndexHandler(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.tmpl", gin.H{})
-}
-
 func main() {
+	cfg, err := config.GetConfigFromJSON("config.json")
 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client, err := mongodb.NewMongoClient(cfg.Mongo.URI)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repository := mongodb.NewMongoRepository(cfg.Mongo.DB, cfg.Mongo.Collection, client)
+
+	storage.SetStorage(repository)
+
+	router := gin.Default()
+	router.GET("/", Index)
+	router.Run()
+}
+
+func Index(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "hello",
+	})
 }
